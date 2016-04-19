9db0fc04413fb4f8ab32d06f3cf5558321b40aa1
RichoDemus <richodemus@gmail.com>
2016-04-19 21:38:24 +0200

    Gradle now builds the docker image

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,1 +1,1 @@
-./gradlew clean build copyDependencies && ./build.sh && docker run -it --rm -p 8080:8080 richodemus/gradle-kotlin-docker-dropwizard:latest
+./gradlew clean build copyDependencies buildImage && docker run -it --rm -p 8080:8080 richodemus/gradle-kotlin-docker-dropwizard:latest
--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t richodemus/gradle-kotlin-docker-dropwizard:latest .
